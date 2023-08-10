55cb5495ad811371436d50cb033438df3af35218
Thaua <thaua19felipe@gmail.com>
2023-08-10 11:14:43 -0300

    create data mock

---
+++ src/Datas.js
@@ -0,0 +1,33 @@
+export const news = [
+    {
+        title: "Notice Title 1 ",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nemo atque dicta consequatur eligendi. Consectetur ipsa dolore et unde porro quasi impedit in, temporibus facere quo praesentium quas inventore quis?",
+        image: "https://www.aberje.com.br/wr-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+
+    {
+        title: "Notice Title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nemo atque dicta consequatur eligendi. Consectetur ipsa dolore et unde porro quasi impedit in, temporibus facere quo praesentium quas inventore quis?",
+        image: "https://www.aberje.com.br/wr-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+
+    {
+        title: "Notice Title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nemo atque dicta consequatur eligendi. Consectetur ipsa dolore et unde porro quasi impedit in, temporibus facere quo praesentium quas inventore quis?",
+        image: "https://www.aberje.com.br/wr-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+
+    {
+        title: "Notice Title 4",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nemo atque dicta consequatur eligendi. Consectetur ipsa dolore et unde porro quasi impedit in, temporibus facere quo praesentium quas inventore quis?",
+        image: "https://www.aberje.com.br/wr-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    }
+]
